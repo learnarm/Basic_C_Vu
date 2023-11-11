6d20bbac9a50c10a0c5e437a93667fca79f68ad2
learnarm <thuong.nguyen35010309@hcmut.edu.vn>
2023-11-11 18:49:53 +0700

    Add

--- lesson_1.c
+++ lesson_1.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
-
-int8_t var; // 8 bit = 1 byte
-
-
-int main() {
-  var = 256;
-  printf("size: %lu byte\n", sizeof(var));
-
-  printf("var = %d\n", var);
-  return 0;
+int test(){
+    return 5;
 }
+
+int var = 5;
+
+void hienThi(uint16_t id, char* ten, uint8_t tuoi){
+
+    printf("ID: %d, TEN: %s, TUOI: %d\n", id, ten, tuoi);
+
+}
+int main() {
+    
+    // printf("ID: 1001, TEN: HOANG, TUOI: 19\n");
+    // printf("ID: 1001, TEN: TUAN, TUOI: 14\n");
+    // printf("ID: 1001, TEN: QUAN, TUOI: 148\n");
+    // printf("ID: 1001, TEN: NAM, TUOI: 17\n");
+    hienThi(1000, "Hoang", 19);
+    return 0;
+}
+
+
+// int32_t var; // 8 bit = 1 byte
+
+// int main() {
+//   var = 256;
+//   printf("size: %lu byte\n", sizeof(var));
+
+//   printf("var = %d\n", var);
+//   return 0;
+// }
