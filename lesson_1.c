e6507b4f21486e2faa65dea35c1522a93b83a678
learnarm <thuong.nguyen35010309@hcmut.edu.vn>
2023-11-11 17:49:20 +0700

    Add

--- lesson_1.c
+++ lesson_1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint8_t var;
+uint8_t var; // 8 bit = 1 byte
 
 
 int main() {
   
-  printf("size: %lu by", sizeof(var));
+  printf("size: %lu byte", sizeof(var));
   return 0;
 }
